d5409540b159c25383887ecd93574d88af28ff90
Rafalopezluna <160245393+Rafalopezluna@users.noreply.github.com>
2024-04-13 22:43:14 -0600

    Add files via upload

---
+++ P1P3/P1P3.js
@@ -0,0 +1,18 @@
+function multi()
+{
+
+    
+    var num; num=prompt('Ingresa un numero(1-10)','');
+    num=parseInt(num);
+    document.write("");
+    var x=1;
+        
+    for (x=1;x<=10;x++)
+    {
+        tabla=num*x;
+        document.write("");
+        document.write("");
+        document.write(num," x ",x," = ",tabla," <br>");
+    }
+    document.write("");
+}
